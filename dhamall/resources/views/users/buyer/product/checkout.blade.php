e8095e58e3c141ed72c4b1828efa6d2642b0d7cb
shah541-g <syedabdulalishah.786@gmail.com>
2025-04-26 12:44:38 +0500

    Still in working

--- dhamall/resources/views/users/buyer/product/checkout.blade.php
+++ dhamall/resources/views/users/buyer/product/checkout.blade.php
@@ -3,9 +3,8 @@
 @section('breadcrumb')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb custom-breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('cart.index') }}">Cart</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Checkout</li>
+            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page" id="breadcrumb-current">Checkout</li>
         </ol>
     </nav>
 @endsection
